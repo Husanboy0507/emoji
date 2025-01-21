36d45ab61095b608e04cce232c01ed4b0ea9509b
Your Name <you@example.com>
2025-01-21 03:50:47 -0800

    changes

--- js/script.js
+++ js/script.js
@@ -1,79 +1,46 @@
-// const apiKey = 'xBaFxhJV205jIXuYqXP3cQ==2co1fd58tu29yXo1'; 
-
- 
-// const url = `https://api.api-ninjas.com/v1/emoji?name=heart` ; // Umumiy emoji so'rovi
-
-// fetch(url, {
-//   method: 'GET',
-//   headers: {
-//     'X-Api-Key': apiKey
-//   }
-// })
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error(`${response.status}`);
-//     }
-//     return response.json();
-//   })
-//   .then(data => {
-//     console.log( data);
-
-//     allEmoji(data)
-
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
-
-
-// const card = document.querySelector('.card')
-// function allEmoji (emoji){
-//   emoji.forEach((item =>{
-//     const emojiitem = document.createElement('div')
-//     emojiitem.classList.add('emojiItem')
-//     const img = document.createElement('img')
-//     img.src = item.image
-//     img.alt = item.name 
-
-//     emojiitem.append(img)
-//     card.append(emojiitem)
-
-
-
-//   }))
-// }
-
-
-
-
 
 
 const apiKey = 'xBaFxhJV205jIXuYqXP3cQ==2co1fd58tu29yXo1'; 
 
-const url = 'https://api.api-ninjas.com/v1/emoji?name=heart'; // Umumiy emoji so'rovi
+const searchInput = document.querySelector('.searchInput')
 
-fetch(url, {
-  method: 'GET',
-  headers: {
-    'X-Api-Key': apiKey
+searchInput.addEventListener('input', () =>{
+  const inputValue = searchInput.value
+  if(inputValue){
+    fetchEmoji(inputValue)
   }
 })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`${response.status}`);
+
+function fetchEmoji (inputValue){
+  
+  const url = 'https://api.api-ninjas.com/v1/emoji?name=heart'; // Umumiy emoji so'rovi
+
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'X-Api-Key': apiKey
     }
-    return response.json();
   })
-  .then(data => {
-    console.log(data);
-    allEmoji(data);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      allEmoji(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  
+
+}
 
 const card = document.querySelector('.card');
 function allEmoji(emoji) {
+  card.innerHTML = '';
   emoji.forEach((item) => {
     const emojiItem = document.createElement('div');
     emojiItem.classList.add('emojiItem');
@@ -86,7 +53,7 @@ function allEmoji(emoji) {
     // Emoji nomi
     const name = document.createElement('p');
     name.textContent = item.name;
-    name.style.marginTop = '5px'; // Styling uchun
+    name.style.marginTop = '5px'; 
 
     // Divga qo'shish
     emojiItem.append(img, name);
